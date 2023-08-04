c12c3d94e4040e43a3a6a9bffbca0536e42855b9
tsoyvi <tsoyvi@inbox.ru>
2023-08-04 16:41:12 +0500

    Верстка главной таблицы

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -43,6 +43,85 @@
       </div>
 
   </div>
+          <table class="table table-bordered table-hover table-striped table-sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th colspan="2">Заказчик</th>
+                <th colspan="5">CO</th>
+                <th rowspan="2">Группа ТУ</th>
+                <th rowspan="2">Способ сварки</th>
+                <th colspan="2">Комиссия</th>
+                <th colspan="2">Заявка</th>
+              </tr>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Заявитель</th>
+                <th scope="col">Организация</th>
+                <th scope="col">#</th>
+                <th scope="col">Наименование</th>
+                <th scope="col">Кол-во, шт.</th>
+                <th scope="col">Шифр</th>
+                <th scope="col">Аттестац.</th>
+                <th scope="col">Приказ</th>
+                <th scope="col">состав</th>
+                <th scope="col">Дата поступления</th>
+                <th scope="col">Регистрация</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>
+                  ООО "СтройИндустрия"
+                  <div class="text-black-50 mt-n1"><small>РФ</small></div>
+                  <div class="text-black-50 mt-n1 mb-n1"><small>5611071117</small></div>
+                </td>
+                <td>ООО «РН-БГПП»</td>
+                <td>1</td>
+                <td>PICO 162</td>
+                <td>2</td>
+                <td>А3</td>
+                <td>первичная</td>
+                <td>РД</td>
+                <td>НГДО, ОХНВП, СК</td>
+                <td>№ 38/СО от 01.08.2023</td>
+                <td>
+                  <div><small>Ракк В.А.</small></div>
+                  <div><small>Ракк Н.В.</small></div>
+                  <div><small>Цой В.И.</small></div>
+                </td>
+                <td>27.07.2023</td>
+                <td>№ АЦСО-123-374 от 01.08.2023</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>
+                  ООО «РН-Бузулукское газоперерабатывающее предприятие»
+                  <div class="text-black-50 mt-n1"><small>РФ</small></div>
+                  <div class="text-black-50 mt-n1 mb-n1"><small>5611071117</small></div>
+                </td>
+                <td>ООО «РН-БГПП»</td>
+                <td>1</td>
+                <td>PICO 162</td>
+                <td>1</td>
+                <td>А3</td>
+                <td>первичная</td>
+                <td>РД</td>
+                <td>НГДО, ОХНВП, СК</td>
+                <td>№ 38/СО от 01.08.2023</td>
+                <td>
+                  <div><small>Ракк В.А.</small></div>
+                  <div><small>Ракк Н.В.</small></div>
+                  <div><small>Цой В.И.</small></div>
+                </td>
+                <td>27.07.2023</td>
+                <td>№ АЦСО-123-374 от 01.08.2023</td>
+              </tr>
+
+            </tbody>
+          </table>
+
 </template>
 
 <script>
@@ -56,5 +135,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.mt-n1 {
+  margin-top: -0.25rem !important;
+}
+.mb-n1 {
+  margin-bottom: -0.25rem !important;
+}
 </style>
